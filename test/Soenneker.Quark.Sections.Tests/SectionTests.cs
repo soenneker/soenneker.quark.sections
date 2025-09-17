4dbbdc2aa2a14369656c85aa8e5f316133bd69dc
Jake Soenneker <jake@soenneker.com>
2025-09-17 16:58:20 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Sections.Tests/SectionTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Sections.Tests;
+
+[Collection("Collection")]
+public sealed class SectionTests : FixturedUnitTest
+{
+    public SectionTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
